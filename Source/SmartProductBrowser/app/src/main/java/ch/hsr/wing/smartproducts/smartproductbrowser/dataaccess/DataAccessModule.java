1370cb0e1ca11a07776a190ea878efe7da82a1d8
Lukas Kretschmar <lukas.kretschmar@hsr.ch>
2019-11-05 23:18:41 +0100

    Added Module for ApiClients

--- Source/SmartProductBrowser/app/src/main/java/ch/hsr/wing/smartproducts/smartproductbrowser/dataaccess/DataAccessModule.java
+++ Source/SmartProductBrowser/app/src/main/java/ch/hsr/wing/smartproducts/smartproductbrowser/dataaccess/DataAccessModule.java
@@ -1,4 +1,17 @@
 package ch.hsr.wing.smartproducts.smartproductbrowser.dataaccess;
 
-public class DataAccessModule {
+import ch.hsr.wing.smartproducts.smartproductbrowser.dataaccess.remote.DataApiClient;
+import ch.hsr.wing.smartproducts.smartproductbrowser.dataaccess.remote.IDataApiClient;
+import ch.hsr.wing.smartproducts.smartproductbrowser.dataaccess.remote.IProductApiClient;
+import ch.hsr.wing.smartproducts.smartproductbrowser.dataaccess.remote.ProductApiClient;
+import dagger.Binds;
+import dagger.Module;
+
+@Module
+public abstract class DataAccessModule {
+    @Binds
+    public abstract IDataApiClient getDataApiClient(DataApiClient client);
+
+    @Binds
+    public abstract IProductApiClient getProductApiClient(ProductApiClient client);
 }
--- Source/SmartProductBrowser/app/src/main/java/ch/hsr/wing/smartproducts/smartproductbrowser/dataaccess/remote/ProductApiClient.java
+++ Source/SmartProductBrowser/app/src/main/java/ch/hsr/wing/smartproducts/smartproductbrowser/dataaccess/remote/ProductApiClient.java
@@ -1,4 +1,11 @@
 package ch.hsr.wing.smartproducts.smartproductbrowser.dataaccess.remote;
 
+import javax.inject.Inject;
+
 public class ProductApiClient implements IProductApiClient {
+
+    @Inject
+    public ProductApiClient(){
+
+    }
 }
