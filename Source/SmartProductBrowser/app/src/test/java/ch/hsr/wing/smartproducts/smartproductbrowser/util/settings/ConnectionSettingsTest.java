43d7c9ba0e0053ec1f2b3df559431e4a211098ce
Lukas Kretschmar <lukas.kretschmar@hsr.ch>
2019-11-05 13:39:01 +0100

    Implemented tests for ConnectionSettings

--- Source/SmartProductBrowser/app/src/test/java/ch/hsr/wing/smartproducts/smartproductbrowser/util/settings/ConnectionSettingsTest.java
+++ Source/SmartProductBrowser/app/src/test/java/ch/hsr/wing/smartproducts/smartproductbrowser/util/settings/ConnectionSettingsTest.java
@@ -8,7 +8,7 @@ import static org.mockito.Mockito.*;
 public class ConnectionSettingsTest {
 
     @Test
-    public void test_getDataEndpoint(){
+    public void test_GetDataEndpoint(){
         IAppSettings appSettings = mock(IAppSettings.class);
         when(appSettings.getString("DataEndpoint")).thenReturn("Data_Endpoint");
 
@@ -17,5 +17,35 @@ public class ConnectionSettingsTest {
         assertEquals("Data_Endpoint", settings.getDataEndpoint());
     }
 
-    
+    @Test
+    public void test_SetDataEndpoint(){
+        IAppSettings appSettings = mock(IAppSettings.class);
+
+        ConnectionSettings settings = new ConnectionSettings(appSettings);
+
+        settings.setDataEndpoint("Data_Endpoint");
+
+        verify(appSettings).setString("DataEndpoint", "Data_Endpoint");
+    }
+
+    @Test
+    public void test_GetProductsEndpoint(){
+        IAppSettings appSettings = mock(IAppSettings.class);
+        when(appSettings.getString("ProductsEndpoint")).thenReturn("Products_Endpoint");
+
+        ConnectionSettings settings = new ConnectionSettings(appSettings);
+
+        assertEquals("Products_Endpoint", settings.getProductsEndpoint());
+    }
+
+    @Test
+    public void test_SetProductsEndpoint(){
+        IAppSettings appSettings = mock(IAppSettings.class);
+
+        ConnectionSettings settings = new ConnectionSettings(appSettings);
+
+        settings.setProductsEndpoint("Products_Endpoint");
+
+        verify(appSettings).setString("ProductsEndpoint", "Products_Endpoint");
+    }
 }
